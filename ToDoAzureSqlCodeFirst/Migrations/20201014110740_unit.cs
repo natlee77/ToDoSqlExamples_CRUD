a810053c76013b3be3815dec533381bb9001cf04
natlee77 <natlisjo@gmail.com>
2020-10-15 11:42:18 +0200

    ToDoSqlExemples_CRUD

---
+++ ToDoAzureSqlCodeFirst/Migrations/20201014110740_unit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoAzureSqlCodeFirst.Migrations
+{
+    public partial class unit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Activity = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Completed = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
---
+++ ToDoAzureSqlCodeFirst/Model/ToDoContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace ToDoAzureSqlCodeFirst.Model
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }//var tabel - kan läggatill new tabel här
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"connect string _local DB");-- local DB
+
+            // optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings[SglMyConnection].ConnectionString); //? funka inte ???
+
+            optionsBuilder.UseSqlServer(@"Server=tcp:sqlserver-ec.database.windows.net,1433;Initial Catalog=AzureSqlCodeFirst;Persist Security Info=False;User ID=natlee;Password=Tindra52;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }//ta bort local connec string
+    //copy connect string från azure och lägg it till app.config--- den file skickas inte in github
+    //her vi referera till app.config
+}
